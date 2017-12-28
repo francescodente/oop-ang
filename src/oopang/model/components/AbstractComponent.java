8455480b0ea256870da0f50f1989aa03899cf645
Francesco Dente <dente.francesco1996@gmail.com>
2017-12-28 03:28:59 +0100

    Create AbstractComponent

---
+++ src/oopang/model/components/AbstractComponent.java
@@ -0,0 +1,57 @@
+package oopang.model.components;
+
+import oopang.model.gameobjects.GameObject;
+
+/**
+ * Generic implementation of the {@link Component} interface.
+ * Defines base behavior that all components share.
+ */
+public abstract class AbstractComponent implements Component {
+
+    private boolean isActive;
+    private final GameObject object;
+
+    /**
+     * Initializes default data for this component.
+     * @param obj
+     *      the game object this component is attached to.
+     */
+    public AbstractComponent(final GameObject obj) {
+        this.isActive = true;
+        this.object = obj;
+    }
+
+    @Override
+    public void start() {
+        // Start is implemented but left empty since not all components may need to override it.
+    }
+
+    @Override
+    public void update(final double deltaTime) {
+        // Update is implemented but left empty since not all components may need to override it.
+    }
+
+    @Override
+    public void enable() {
+        this.isActive = true;
+    }
+
+    @Override
+    public void disable() {
+        this.isActive = false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isActive;
+    }
+
+    /**
+     * Gets the game object this component is attached to.
+     * @return
+     *      the game object.
+     */
+    protected GameObject getGameObject() {
+        return this.object;
+    }
+}
