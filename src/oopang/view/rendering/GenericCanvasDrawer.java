38baf3e6f7b6be927115a72da75babc8d4a72e12
Francesco Dente <dente.francesco1996@gmail.com>
2018-02-16 04:26:17 +0100

    Create GenericCanvasDrawer

---
+++ src/oopang/view/rendering/GenericCanvasDrawer.java
@@ -0,0 +1,44 @@
+package oopang.view.rendering;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * A generic canvas drawer that gives basic implementation of the {@link CanvasDrawer} interface.
+ */
+public abstract class GenericCanvasDrawer implements CanvasDrawer {
+
+    private final List<Renderer> renderers;
+
+    /**
+     * Creates a new Canvas drawer with generic.
+     */
+    public GenericCanvasDrawer() {
+        this.renderers = new ArrayList<>();
+    }
+
+    @Override
+    public void draw() {
+        this.getRenderers().forEach(Renderer::render);
+    }
+
+    @Override
+    public void addRenderer(final Renderer rend) {
+        this.renderers.add(rend);
+    }
+
+    @Override
+    public void removeRenderer(final Renderer rend) {
+        this.renderers.remove(rend);
+    }
+
+    /**
+     * Returns an ordered stream of all the renderers that currently need to be rendered.
+     * @return
+     *      the stream of all renderers.
+     */
+    protected Stream<Renderer> getRenderers() {
+        return this.renderers.stream().sorted();
+    }
+}
