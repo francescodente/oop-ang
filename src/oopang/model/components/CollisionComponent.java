f584dd722c635ae31586c2bf5102a8a3062016be
Francesco Dente <dente.francesco1996@gmail.com>
2018-02-23 17:39:16 +0100

    Fix checkstyle issues in CollisionComponent

--- src/oopang/model/components/CollisionComponent.java
+++ src/oopang/model/components/CollisionComponent.java
@@ -2,28 +2,39 @@ package oopang.model.components;
 
 import org.dyn4j.geometry.Convex;
 
-import oopang.commons.events.*;
-import oopang.commons.space.*;
+import oopang.commons.events.Event;
+import oopang.commons.events.EventHandler;
+import oopang.commons.space.Point2D;
 import oopang.model.gameobjects.GameObject;
 import oopang.model.physics.Collidable;
 import oopang.model.physics.Collision;
 
+/**
+ * A component that makes the {@link GameObject} it is attached to able to collide with other objects.
+ */
 public class CollisionComponent extends AbstractComponent implements Collidable {
 
-    private Convex boundingBox;
-    private Event<Collision> collisionEvent;
-    
-    public CollisionComponent(GameObject obj, Convex boundingBox) {
+    private final Convex boundingBox;
+    private final Event<Collision> collisionEvent;
+
+    /**
+     * Creates a new Collision component with the given bounding box.
+     * @param obj
+     *      the game object this component is attached to.
+     * @param boundingBox
+     *      the convex shape of the object.
+     */
+    public CollisionComponent(final GameObject obj, final Convex boundingBox) {
         super(obj);
         this.boundingBox = boundingBox;
-        collisionEvent = new Event<>();
+        this.collisionEvent = new Event<>();
     }
-    
+
     @Override
     public void start() {
         //CollisionManager.addCollidable(this);
     }
-    
+
     @Override
     public Convex getShape() {
         return this.boundingBox;
@@ -35,19 +46,17 @@ public class CollisionComponent extends AbstractComponent implements Collidable 
     }
 
     @Override
-    public void notifyCollision(Collision coll) {
-        collisionEvent.trigger(coll);
+    public void notifyCollision(final Collision coll) {
+        this.collisionEvent.trigger(coll);
     }
 
     @Override
-    public void registerCollisionEvent(EventHandler<Collision> handler) {
-        collisionEvent.registerHandler(handler);
-        
+    public void registerCollisionEvent(final EventHandler<Collision> handler) {
+        this.collisionEvent.registerHandler(handler);
     }
 
     @Override
-    public void unregisterCollisionEvent(EventHandler<Collision> handler) {
-        collisionEvent.unregisterHandler(handler);
+    public void unregisterCollisionEvent(final EventHandler<Collision> handler) {
+        this.collisionEvent.unregisterHandler(handler);
     }
-
 }
