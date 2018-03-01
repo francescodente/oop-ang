aa31040a758cc16b02f0b8ac95a82010ae1c8c23
Samuele Burattini <samuele.burattini@studio.unibo.it>
2018-03-01 16:13:43 +0100

    Add setCurrentLevel in Model interface and impl

--- src/oopang/model/World.java
+++ src/oopang/model/World.java
@@ -1,5 +1,8 @@
 package oopang.model;
 
+import oopang.commons.LevelManager;
+import oopang.model.levels.Level;
+
 /**
  * This class is the concrete implementation of the Model interface.
  * It contains the basic methods for loading new levels and update.
@@ -9,7 +12,12 @@ public class World implements Model {
 
     @Override
     public void update(final double deltaTime) {
-        //currentLevel.ifPresent(l -> l.update(deltaTime));
+        LevelManager.getCurrentLevel().update(deltaTime);
+    }
+
+    @Override
+    public void setCurrentLevel(final Level level) {
+        LevelManager.setCurrentLevel(level);
     }
 
 }
