0077130e9b5e575706d5df2ada24fda3786447d5
Francesco Dente <dente.francesco1996@gmail.com>
2018-03-01 17:54:52 +0100

    Create CollisionTag enum

---
+++ src/oopang/model/physics/CollisionTag.java
@@ -0,0 +1,39 @@
+package oopang.model.physics;
+
+/**
+ * Represents a value used to decide if an object type can collide with another.
+ */
+public enum CollisionTag {
+    /**
+     * Collision tag for the player object.
+     */
+    PLAYER,
+    /**
+     * Collision tag for all bubbles.
+     */
+    BUBBLE,
+    /**
+     * Collision tag for the floor.
+     */
+    FLOOR,
+    /**
+     * Collision tag for the ceiling.
+     */
+    CEILING,
+    /**
+     * Collision tag for the left wall.
+     */
+    LEFT_WALL,
+    /**
+     * Collision tag for the right wall.
+     */
+    RIGHT_WALL,
+    /**
+     * Collision tag for all pickups.
+     */
+    PICKUP,
+    /**
+     * Collision tag for all shots.
+     */
+    SHOT;
+}
