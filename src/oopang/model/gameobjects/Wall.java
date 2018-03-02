620573be167244331844082b07b6d84865b7d276
Francesco Dente <dente.francesco1996@gmail.com>
2018-03-02 14:36:37 +0100

    Create Wall gameObject

---
+++ src/oopang/model/gameobjects/Wall.java
@@ -0,0 +1,36 @@
+package oopang.model.gameobjects;
+
+import java.util.stream.Stream;
+
+import org.dyn4j.geometry.Convex;
+import org.dyn4j.geometry.Rectangle;
+
+import oopang.model.components.CollisionComponent;
+import oopang.model.components.Component;
+import oopang.model.physics.CollisionTag;
+
+/**
+ * Represents a wall in the world.
+ */
+public class Wall extends AbstractGameObject {
+
+    private final Component collision;
+
+    /**
+     * Creates a new Wall {@link GameObject} given its dimensions.
+     * @param width
+     *      the width of the wall.
+     * @param height
+     *      the height of the wall.
+     */
+    public Wall(final double width, final double height) {
+        super();
+        final Convex shape = new Rectangle(width, height);
+        this.collision = new CollisionComponent(this, shape, CollisionTag.WALL);
+    }
+
+    @Override
+    public Stream<Component> getAllComponents() {
+        return Stream.of(this.collision);
+    }
+}
