898fb4c7e3290f0cf4e0bcec98382b23309efa7a
Samuele Burattini <samuele.burattini@studio.unibo.it>
2018-03-06 18:36:40 +0100

    Start building ControllerImpl class

---
+++ src/oopang/controller/ControllerImpl.java
@@ -0,0 +1,51 @@
+package oopang.controller;
+
+import oopang.commons.Command;
+import oopang.model.Model;
+import oopang.model.World;
+import oopang.view.View;
+
+/**
+ * This is the concrete implementation of the Controller.
+ */
+public class ControllerImpl implements Controller {
+
+    private final Model model;
+    private final View view;
+    private final GameLoop loop;
+    //private final LevelLoader loader;
+
+    /**
+     * Create a new Controller instance.
+     */
+    public ControllerImpl() {
+        model = new World();
+        //view = new ViewImpl??
+        loop = new GameLoop(view, model);
+    }
+
+    @Override
+    public final void startGame(final int levelIndex) {
+        //Level level = loader.load(levelIndex);
+        //model.setCurrentLevel(level);
+        loop.start();
+    }
+
+    @Override
+    public void pauseGame() {
+        this.loop.pauseLoop();
+    }
+
+    @Override
+    public void closeGame() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void sendCommand(final Command cmd) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
