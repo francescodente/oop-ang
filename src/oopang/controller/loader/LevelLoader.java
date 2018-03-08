d15b4ab8d0a89de7633503e076a1877477369724
Lorenzo <lorenzo.menghini2@studio.unibo.it>
2018-03-08 18:12:13 +0100

    Creation of the interface LevelLoader

---
+++ src/oopang/controller/loader/LevelLoader.java
@@ -0,0 +1,12 @@
+package oopang.controller.loader;
+
+/**
+ * The interface that controls the loading of the selected level into the game.
+ */
+public interface LevelLoader {
+    /**
+     * Method that load the level.
+     * N.B. Need to decide what kind of parameter the method need
+     */
+    public void load();
+}
