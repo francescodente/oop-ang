50f172d14c45de76b6194cf5da1b4dabe49c9aee
Lorenzo <lorenzo.menghini2@studio.unibo.it>
2018-03-08 18:29:23 +0100

    Create the enum LevelTag

---
+++ src/oopang/controller/loader/LevelTag.java
@@ -0,0 +1,18 @@
+package oopang.controller.loader;
+/**
+ * Annum representing the kind of levels could be loaded.
+ */
+public enum LevelTag {
+    /**
+     * An endless level.
+     */
+    INFINITE_LEVEL,
+    /**
+     * Tutorial level.
+     */
+    TUTORIAL,
+    /**
+     * Story mode level.
+     */
+    STORY;
+}
