3b37ac710f57bdbbb5121bd2bc443234cb60df5c
Francesco Dente <dente.francesco1996@gmail.com>
2018-03-08 17:39:39 +0100

    Create LevelDecorator

---
+++ src/oopang/model/levels/LevelDecorator.java
@@ -0,0 +1,77 @@
+package oopang.model.levels;
+
+import java.util.stream.Stream;
+
+import oopang.commons.events.EventHandler;
+import oopang.model.gameobjects.GameObject;
+import oopang.model.gameobjects.GameObjectFactory;
+import oopang.model.physics.CollisionManager;
+
+/**
+ * Represents an abstraction for all level decorators. Every method of the {@link Level} interface
+ * is implemented by delegating to the decorated instance of level. Thus, subclasses may access
+ * the decorated object by calling this class' methods.
+ */
+public abstract class LevelDecorator implements Level {
+
+    private final Level innerLevel;
+
+    /**
+     * Initializes this decorator with the given Level object.
+     * @param baseLevel
+     *      the level to decorate.
+     */
+    public LevelDecorator(final Level baseLevel) {
+        this.innerLevel = baseLevel;
+    }
+
+    @Override
+    public void start() {
+        this.innerLevel.start();
+    }
+
+    @Override
+    public void update(final double deltaTime) {
+        this.innerLevel.update(deltaTime);
+    }
+
+    @Override
+    public Stream<GameObject> getAllObjects() {
+        return this.innerLevel.getAllObjects();
+    }
+
+    @Override
+    public void addGameObject(final GameObject obj) {
+        this.innerLevel.addGameObject(obj);
+    }
+
+    @Override
+    public void removeGameObject(final GameObject obj) {
+        this.innerLevel.removeGameObject(obj);
+    }
+
+    @Override
+    public int getScore() {
+        return this.innerLevel.getScore();
+    }
+
+    @Override
+    public GameObjectFactory getGameObjectFactory() {
+        return this.innerLevel.getGameObjectFactory();
+    }
+
+    @Override
+    public CollisionManager getCollisionManager() {
+        return this.innerLevel.getCollisionManager();
+    }
+
+    @Override
+    public void registerObjectCreatedEvent(final EventHandler<GameObject> handler) {
+        this.innerLevel.registerObjectCreatedEvent(handler);
+    }
+
+    @Override
+    public void unregisterObjectCreatedEvent(final EventHandler<GameObject> handler) {
+        this.innerLevel.unregisterObjectCreatedEvent(handler);
+    }
+}
