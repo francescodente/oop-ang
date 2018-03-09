85951f690021b4f97c401a4fff0c49c44063bda7
Samuele Burattini <samuele.burattini@studio.unibo.it>
2018-03-09 15:04:08 +0100

    Create empty class ViewImpl and add method notifyNewGameObject

---
+++ src/oopang/view/ViewImpl.java
@@ -0,0 +1,43 @@
+package oopang.view;
+
+import oopang.model.gameobjects.GameObject;
+
+/**
+ * This is the concrete implementation of the view Interface.
+ *
+ */
+public class ViewImpl implements View {
+
+    /**
+     * Create a new ViewImpl instance.
+     */
+    public ViewImpl() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void launch() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void render() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void loadScene(final GameScene scene) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void showPauseMenu() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void notifyNewGameObject(final GameObject obj) {
+
+    }
+
+}
