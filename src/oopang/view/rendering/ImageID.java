e8dae812ee5525c4560a7cf0fb9bd8154ac04ea1
Francesco Dente <dente.francesco1996@gmail.com>
2018-03-12 20:03:24 +0100

    Add new image IDs

--- src/oopang/view/rendering/ImageID.java
+++ src/oopang/view/rendering/ImageID.java
@@ -7,7 +7,19 @@ public enum ImageID {
     /**
      * The image for the player character.
      */
-    PLAYER("");
+    PLAYER(""),
+    /**
+     * The image for the balls.
+     */
+    BALL(""),
+    /**
+     * The image for the walls.
+     */
+    WALL(""),
+    /**
+     * The image for the backgrounds.
+     */
+    BACKGROUND("");
 
     private final String path;
 
