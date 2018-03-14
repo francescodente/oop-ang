681c9208ca863595be55cf9b9ceaede32958dd5e
Francesco Dente <dente.francesco1996@gmail.com>
2018-03-14 13:14:25 +0100

    Modify View interface

--- src/oopang/view/View.java
+++ src/oopang/view/View.java
@@ -1,5 +1,6 @@
 package oopang.view;
 
+import oopang.controller.Controller;
 import oopang.model.gameobjects.GameObject;
 
 /**
@@ -8,34 +9,33 @@ import oopang.model.gameobjects.GameObject;
 public interface View {
 
     /**
-     * 
+     * Makes the UI visible and sets the controller to be used.
+     * @param controller
+     *      the {@link Controller} object.
      */
-    void launch();
+    void launch(Controller controller);
 
     /**
-     * 
+     * Tells the view to draw the current state of the game.
      */
     void render();
 
     /**
-     * 
+     * Changes the currently visualized menu.
      * @param scene
-     *      value for the selected scene to be loaded
+     *      value for the selected scene to be loaded.
      */
     void loadScene(GameScene scene);
 
     /**
-     * 
+     * Shows the pause menu as an overlay of the game view.
      */
     void showPauseMenu();
 
     /**
      * Create the new GameObject renderer and register to its destruction.
      * @param obj
-     *      the new GameObject added to the level
+     *      the new GameObject added to the level.
      */
     void notifyNewGameObject(GameObject obj);
-
-    //TODO event impl
-
 }
