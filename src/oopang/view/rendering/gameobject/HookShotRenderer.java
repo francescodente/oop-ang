7a7f1a3f8a0d43462d98f9f477db446507d220ff
Samuele Burattini <samuele.burattini@studio.unibo.it>
2018-03-14 11:17:52 +0100

    Fix HookShotRenderer now ok

--- src/oopang/view/rendering/gameobject/HookShotRenderer.java
+++ src/oopang/view/rendering/gameobject/HookShotRenderer.java
@@ -3,7 +3,6 @@ package oopang.view.rendering.gameobject;
 import oopang.commons.space.Points2D;
 import oopang.commons.space.Vectors2D;
 import oopang.model.gameobjects.HookShot;
-import oopang.model.gameobjects.Shot;
 import oopang.model.gameobjects.StickyShot;
 import oopang.view.rendering.ImageID;
 import oopang.view.rendering.Sprite;
@@ -13,10 +12,9 @@ import oopang.view.rendering.Sprite;
  * Represents a Renderer for {@link Shot} {@link GameObject}s.
  *
  */
-public class ShotRenderer extends GameObjectRenderer<Shot> {
+public class HookShotRenderer extends GameObjectRenderer<HookShot> {
 
     private static final double MAX_SHOT_HEIGHT = 100;
-    private static final double MAX_SHOT_WIDTH = 10;
     private static final int SHOT_LAYER = 1; 
     //TODO almost certainly wrong.
 
@@ -27,25 +25,25 @@ public class ShotRenderer extends GameObjectRenderer<Shot> {
      * @param gameObject
      *      The {@link Shot} {@link GameObject}.
      */
-    public ShotRenderer(final Sprite sprite, final Shot gameObject) {
+    public HookShotRenderer(final Sprite sprite, final HookShot gameObject) {
         super(sprite, gameObject);
         this.setLayer(SHOT_LAYER);
 
-        if (gameObject instanceof HookShot) {
-            sprite.setSource(ImageID.HOOKSHOT);
-        } else if (gameObject instanceof StickyShot) {
+        if (gameObject instanceof StickyShot) {
             sprite.setSource(ImageID.STICKYSHOT);
+        } else {
+            sprite.setSource(ImageID.HOOKSHOT);
         }
 
-        final double width = gameObject.getWidth() * sprite.getSourceWidth() / MAX_SHOT_WIDTH;
+        final double width = sprite.getSourceWidth();
         final double height = gameObject.getHeight() * sprite.getSourceHeight() / MAX_SHOT_HEIGHT;
         sprite.setSourceWindow(Points2D.ZERO, Vectors2D.of(width, height));
-        //sprite.setPivot(????);
+        sprite.setPivot(Vectors2D.of(0, 1));
     }
 
     @Override
     public void render() {
-        final double width = getGameObject().getWidth() * getSprite().getSourceWidth() / MAX_SHOT_WIDTH;
+        final double width = getSprite().getSourceWidth();
         final double height = getGameObject().getHeight() * getSprite().getSourceHeight() / MAX_SHOT_HEIGHT;
         getSprite().setSourceWindow(Points2D.ZERO, Vectors2D.of(width, height));
         super.render();
