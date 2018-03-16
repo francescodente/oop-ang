2047bee5b509bb11352f2bf894033730dfd0fce8
Samuele Burattini <samuele.burattini@studio.unibo.it>
2018-03-16 14:11:49 +0100

    Minor changes in GameSession constructor

--- src/oopang/controller/GameSession.java
+++ src/oopang/controller/GameSession.java
@@ -1,5 +1,7 @@
 package oopang.controller;
 
+import oopang.controller.loader.SessionTag;
+
 /**
  * Class including informations about the current session game.
  */
@@ -15,8 +17,10 @@ public class GameSession {
      * Constructor of the class.
      * @param multi
      *      parameter specifying if is a multiplayer session.
+     * @param tag
+     *      parameter specifying the game mode to be chose.
      */
-    public GameSession(final boolean multi) {
+    public GameSession(final SessionTag tag, final boolean multi) {
         this.lifes = DEFAULT;
         this.score = DEFAULT;
         this.isMultiplayer = multi;
