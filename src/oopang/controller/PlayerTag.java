80f5ddcf7a1ae21eb77f81b8d7ef48acc97cd88e
Samuele Burattini <samuele.burattini@studio.unibo.it>
2018-03-16 14:12:01 +0100

    Create playerTag enum

---
+++ src/oopang/controller/PlayerTag.java
@@ -0,0 +1,16 @@
+package oopang.controller;
+
+/**
+ * This enum identifies the player.
+ *
+ */
+public enum PlayerTag {
+    /**
+     * The first player, used in singlePlayer Mode.
+     */
+    PLAYER_ONE,
+    /**
+     * The second player, used only in multiplayer mode.
+     */
+    PLAYER_TWO;
+}
