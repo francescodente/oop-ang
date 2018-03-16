aa301a06896c954e7f348b848f035f374205da86
Lorenzo <lorenzo.menghini2@studio.unibo.it>
2018-03-16 23:42:31 +0100

    Create InfiniteGameSession class

---
+++ src/oopang/controller/InfiniteGameSession.java
@@ -0,0 +1,39 @@
+package oopang.controller;
+
+import oopang.model.GameOverStatus;
+import oopang.model.LevelResult;
+import oopang.model.Model;
+import oopang.view.View;
+
+/**
+ * Class representing a InfiniteGameSession.
+ */
+public final class InfiniteGameSession extends GameSession {
+
+    /**
+     * Constructor of the infinite {@link GameSession).
+     * @param view
+     *      The {@link view} of the Game
+     * @param model
+     *      The {@link Model) of the Game
+     * @param isMultiPlayer
+     *      Boolean representing if is or not multi player session.
+     */
+    public InfiniteGameSession(final View view, final Model model, final boolean isMultiPlayer) {
+        super(view, model, isMultiPlayer);
+    }
+
+    @Override
+    public void continueGame() {
+    }
+
+    @Override
+    protected void handleGameOver(final GameOverStatus status) {
+       final LevelResult result = status.getResult();
+       if (result == LevelResult.PLAYER_DEAD) {
+           super.addScore(status.getScore());
+           this.triggerShouldEnd(true);
+       }
+    }
+
+}
