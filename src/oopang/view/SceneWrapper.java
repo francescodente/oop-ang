121349ebdcac392e5ba284b9e145b855d7615fee
Samuele Burattini <samuele.burattini@studio.unibo.it>
2018-03-20 21:31:28 +0100

    Create SceneLoader and Scene Wrapper objects

---
+++ src/oopang/view/SceneWrapper.java
@@ -0,0 +1,43 @@
+package oopang.view;
+
+import javafx.scene.Scene;
+
+/**
+ * This object wraps a Scene with its associated controller.
+ */
+public class SceneWrapper {
+
+    private final Scene scene;
+    private final SceneController controller;
+
+    /**
+     * Create a new Scene Wrapper.
+     * @param scene
+     *      the scene object
+     * @param controller
+     *      the controller associated controller
+     */
+    public SceneWrapper(final Scene scene, final SceneController controller) {
+        this.scene = scene;
+        this.controller = controller;
+    }
+
+    /**
+     * Return the Scene.
+     * @return
+     *      the Scene object
+     */
+    public Scene getScene() {
+        return this.scene;
+    }
+
+    /**
+     * Returns the controller of the Scene.
+     * @return
+     *      the Scene Controller.
+     */
+    public SceneController getController() {
+        return this.controller;
+    }
+
+}
