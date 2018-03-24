8f8f2dbac4ffa6923d15a62ff87fde9eeb8af5fa
Lorenzo <lorenzo.menghini2@studio.unibo.it>
2018-03-24 15:57:38 +0100

    Implementing GameGuiController

--- src/oopang/view/GameController.java
+++ src/oopang/view/GameController.java
@@ -1,5 +1,10 @@
 package oopang.view;
 
+import java.awt.event.KeyEvent;
+
+import oopang.commons.Command;
+import oopang.controller.PlayerTag;
+import oopang.model.input.InputDirection;
 import oopang.model.input.InputWriter;
 import oopang.view.rendering.CanvasDrawer;
 
@@ -10,9 +15,28 @@ public class GameController extends SceneController {
 
     private CanvasDrawer canvasDrawer;
     private InputWriter input;
+
     
+    public GameController(CanvasDrawer canvasDrawer, InputWriter input) {
+        super();
+        this.canvasDrawer = canvasDrawer;
+        this.input = input;
+    }
+
     @Override
     protected void nextScene() {
         // TODO Auto-generated method stub
+        this.getController().closeGameSession();
+    }
+
+    public void handleInput(KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.VK_LEFT) {
+            input.setDirection(InputDirection.LEFT);
+            this.getController().sendCommand(, PlayerTag.PLAYER_ONE);
+        }
+    }
+    
+    public void render() {
+        canvasDrawer.addRenderer(rend);
     }
 }
