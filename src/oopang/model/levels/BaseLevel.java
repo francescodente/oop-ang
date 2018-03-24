9d5714486e89603f8372f823e4171c5d7dc58f81
Samuele Burattini <samuele.burattini@studio.unibo.it>
2018-03-24 11:39:23 +0100

    Add private method for externalWall generation in BaseLevel

--- src/oopang/model/levels/BaseLevel.java
+++ src/oopang/model/levels/BaseLevel.java
@@ -1,13 +1,16 @@
 package oopang.model.levels;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 import java.util.List;
 import java.util.Queue;
 import java.util.stream.Stream;
 
+import oopang.commons.LevelManager;
 import oopang.commons.events.Event;
 import oopang.commons.events.EventHandler;
+import oopang.commons.space.Points2D;
 import oopang.model.GameOverStatus;
 import oopang.model.gameobjects.BasicFactory;
 import oopang.model.gameobjects.GameObject;
@@ -22,6 +25,9 @@ import oopang.model.physics.SimpleCollisionManager;
 public class BaseLevel implements Level {
 
     private static final int INITIAL_SCORE = 0;
+    private static final double WORLD_WIDTH = 200;
+    private static final double WORLD_HEIGHT = 100;
+    private static final double WALL_WIDTH = 4;
 
     private final Queue<GameObject> startQueue;
     private final List<GameObject> gameObjects;
@@ -29,6 +35,7 @@ public class BaseLevel implements Level {
     private final GameObjectFactory factory;
     private final CollisionManager collisionManager;
     private final Event<GameObject> objectCreatedEvent;
+    private final List<GameObject> externalwalls;
 
     /**
      * Creates a new BaseLevel object.
@@ -40,10 +47,12 @@ public class BaseLevel implements Level {
         this.factory = new BasicFactory(this);
         this.collisionManager = new SimpleCollisionManager();
         this.objectCreatedEvent = new Event<>();
+        this.externalwalls = this.createWalls();
     }
 
     @Override
     public void start() {
+        this.externalwalls.forEach(w -> this.gameObjects.add(w));
         this.gameObjects.forEach(e -> e.start());
     }
 
@@ -106,5 +115,17 @@ public class BaseLevel implements Level {
     @Override
     public void registerGameOverEvent(final EventHandler<GameOverStatus> handler) {
         // A base level is not able to end.
+    }
+
+    private List<GameObject> createWalls() {
+        final GameObject horizontal1 = LevelManager.getCurrentLevel().getGameObjectFactory().createWall(WORLD_WIDTH, WALL_WIDTH);
+        horizontal1.setPosition(Points2D.of(0, -WALL_WIDTH / 2));
+        final GameObject horizontal2 = LevelManager.getCurrentLevel().getGameObjectFactory().createWall(WORLD_WIDTH, WALL_WIDTH);
+        horizontal2.setPosition(Points2D.of(0, WORLD_HEIGHT + (WALL_WIDTH / 2)));
+        final GameObject vertical1 = LevelManager.getCurrentLevel().getGameObjectFactory().createWall(WALL_WIDTH, WORLD_HEIGHT);
+        vertical1.setPosition(Points2D.of(-((WORLD_WIDTH / 2) + (WALL_WIDTH / 2)), WORLD_HEIGHT / 2));
+        final GameObject vertical2 = LevelManager.getCurrentLevel().getGameObjectFactory().createWall(WALL_WIDTH, WORLD_HEIGHT);
+        vertical2.setPosition(Points2D.of((WORLD_WIDTH / 2) + (WALL_WIDTH / 2), WORLD_HEIGHT / 2));
+        return Arrays.asList(horizontal1, horizontal2, vertical1, vertical2);
     }
 }
