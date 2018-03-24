cdf97800b68d8813b8c5afb723193b92353b7629
Lorenzo <lorenzo.menghini2@studio.unibo.it>
2018-03-24 15:24:46 +0100

    Create Controller for GameGui Scene

---
+++ src/oopang/view/GameController.java
@@ -0,0 +1,18 @@
+package oopang.view;
+
+import oopang.model.input.InputWriter;
+import oopang.view.rendering.CanvasDrawer;
+
+/**
+ * Class implementing the real scene of the game.
+ */
+public class GameController extends SceneController {
+
+    private CanvasDrawer canvasDrawer;
+    private InputWriter input;
+    
+    @Override
+    protected void nextScene() {
+        // TODO Auto-generated method stub
+    }
+}
