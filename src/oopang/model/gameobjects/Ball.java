823eae6bde46571006fc1243aea06bbb7b2feae5
Francesco Dente <dente.francesco1996@gmail.com>
2018-04-06 12:55:35 +0200

    Take in account ball radius during bounce

--- src/oopang/model/gameobjects/Ball.java
+++ src/oopang/model/gameobjects/Ball.java
@@ -107,7 +107,7 @@ public class Ball extends AbstractGameObject {
         if (this.size != MIN_BALL_SIZE) {
             yvalue--;
         }
-        return this.size * yvalue - this.getPosition().getY();
+        return this.size * yvalue - this.getPosition().getY() + this.radius;
     }
 
     /**
