3617cf945e5d672f38e745763e7e8050fc05826d
Samuele Burattini <samuele.burattini@studio.unibo.it>
2018-04-07 17:36:59 +0200

    Now Shield creates a bubble around the player
    Not finished the ball does not disappear when power is over

--- src/oopang/view/rendering/gameobject/PlayerRenderer.java
+++ src/oopang/view/rendering/gameobject/PlayerRenderer.java
@@ -1,8 +1,11 @@
 package oopang.view.rendering.gameobject;
 
+import java.util.Optional;
+
 import oopang.commons.PlayerTag;
 import oopang.commons.space.Vectors2D;
 import oopang.model.gameobjects.Player;
+import oopang.model.powers.PowerTag;
 import oopang.view.rendering.CanvasDrawer;
 import oopang.view.rendering.ImageID;
 import oopang.view.rendering.Sprite;
@@ -13,7 +16,10 @@ import oopang.view.rendering.Sprite;
 public class PlayerRenderer extends GameObjectRenderer<Player> {
 
     private static final int PLAYER_LAYER = 3;
-    private static final double OFFSET = 3;
+    private static final double PLAYER_OFFSET = 3;
+    private static final double SHIELD_H_OFFSET = 8;
+    private static final double SHIELD_V_OFFSET = 5;
+    private Optional<Sprite> shield;
     /**
      * Constructor of the {@link Player} Renderer.
      * @param sprite
@@ -25,6 +31,7 @@ public class PlayerRenderer extends GameObjectRenderer<Player> {
      */
     public PlayerRenderer(final Sprite sprite, final Player gameObject, final CanvasDrawer canvasDrawer) {
         super(sprite, gameObject);
+        shield = Optional.empty();
         this.setLayer(PLAYER_LAYER);
         if (gameObject.getPlayerTag() == PlayerTag.PLAYER_ONE) {
             sprite.setSource(ImageID.PLAYER1);
@@ -32,12 +39,28 @@ public class PlayerRenderer extends GameObjectRenderer<Player> {
             sprite.setSource(ImageID.PLAYER2);
         }
         sprite.setPivot(Vectors2D.of(0, -1));
+        gameObject.getPickupCollectedEvent().register(p -> {
+            if (p.getPowertag() == PowerTag.TIMEDSHIELD) {
+                final Sprite shieldsprite = canvasDrawer.getRendererFactory().createSprite();
+                shieldsprite.setSource(ImageID.SHIELD);
+                shieldsprite.setPivot(Vectors2D.of(0, -1));
+                shieldsprite.setLayer(PLAYER_LAYER + 1);
+                shieldsprite.setWidth(this.getGameObject().getWidth() + SHIELD_H_OFFSET);
+                shieldsprite.setHeight(this.getGameObject().getHeight() + SHIELD_V_OFFSET);
+                canvasDrawer.addRenderer(shieldsprite);
+                this.shield = Optional.of(shieldsprite);
+            }
+        });
     }
 
     @Override
     public void render() {
+        if (shield.isPresent()) {
+            final Sprite shieldsprite = this.shield.get();
+            shieldsprite.setPosition(this.getGameObject().getPosition());
+        }
         this.getSprite().setPosition(this.getGameObject().getPosition());
-        this.getSprite().setWidth(this.getGameObject().getWidth() + OFFSET);
+        this.getSprite().setWidth(this.getGameObject().getWidth() + PLAYER_OFFSET);
         this.getSprite().setHeight(this.getGameObject().getHeight());
         this.getSprite().render();
     }
