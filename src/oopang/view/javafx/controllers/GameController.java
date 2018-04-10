9929905d53d33068e72db6b8c25b8843f069e51f
Francesco Dente <dente.francesco1996@gmail.com>
2018-04-10 14:35:23 +0200

    Fix PMD warning

--- src/oopang/view/javafx/controllers/GameController.java
+++ src/oopang/view/javafx/controllers/GameController.java
@@ -1,6 +1,5 @@
 package oopang.view.javafx.controllers;
 
-import javafx.beans.binding.StringBinding;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
