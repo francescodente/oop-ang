adafcadbfe9b5b4aea7e428e69d4988dca90856f
Nicholas Ciarafoni <nicholas.ciarafoni@studio.unibo.it>
2018-04-14 21:35:07 +0200

    modify powertag

--- src/oopang/model/powers/PowerTag.java
+++ src/oopang/model/powers/PowerTag.java
@@ -1,4 +1,8 @@
 package oopang.model.powers;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Enum describing the power tag.
  *
@@ -7,25 +11,40 @@ public enum PowerTag {
     /**
      * Tag AdhesiveShot.
      */
-    ADHESIVESHOT,
+    ADHESIVESHOT(Arrays.asList(2000)),
     /**
      * Tag DoubleShot.
      */
-    DOUBLESHOT,
+    DOUBLESHOT(Arrays.asList(2000)),
     /**
      * Tag DoubleSpeed.
      */
-    DOUBLESPEED,
+    DOUBLESPEED(Arrays.asList(100, 200, 500, 1000)),
     /**
      * Tag TimedShield.
      */
-    TIMEDSHIELD,
+    TIMEDSHIELD(Arrays.asList(100, 200, 500, 1000)),
     /**
      * Tag Dynamite.
      */
-    DYNAMITE,
+    DYNAMITE(Arrays.asList(500, 1000, 2000)),
     /**
      * Tag Freeze.
      */
-    FREEZE;
+    FREEZE(Arrays.asList(100, 200, 500, 1000));
+
+    private  final List<Integer> list;
+    PowerTag(final List<Integer> list) {
+        this.list = list;
+    }
+    /**
+     * Getter for list.
+     * @param x
+     *      Index
+     * @return 
+     *      Index.
+     */
+    public int getCost(int x) {
+        return list.get(x - 1);
+    }
 }
