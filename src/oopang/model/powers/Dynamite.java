359e09f7df53ad99b6931086495ac3c5eee91126
Nicholas Ciarafoni <nicholas.ciarafoni@studio.unibo.it>
2018-04-14 22:31:26 +0200

    Modify dynamite power

--- src/oopang/model/powers/Dynamite.java
+++ src/oopang/model/powers/Dynamite.java
@@ -9,26 +9,26 @@ import oopang.model.levels.LevelManager;
  * This enhancement splits all the balls in the level.
  *
  */
-public final class Dynamite extends PowerInstant {
+public final class Dynamite extends AbstractPower {
     private static final PowerTag TAG = PowerTag.DYNAMITE;
+    private static final double WAIT_TIME = 0.5;
+    private int remainingHits;
+    private double timeToNextDestroy;
+
     /**
      * The constructor for the dynamite class.
+     * @param powerlevel
+     *      The number of times to destroy balls.
      */
-    public Dynamite() {
+    public Dynamite(final int powerlevel) {
         super(TAG);
+        this.remainingHits = powerlevel;
     }
 
     @Override
     public void activate(final Player player) {
         super.activate(player);
-        LevelManager.getCurrentLevel().getAllObjects().forEach(obj ->
-        obj.accept(new AbstractGameObjectVisitor<Void>(null) {
-            @Override
-            public Void visit(final Ball ball) {
-                ball.destroy();
-                return null;
-            }
-        }));
+        this.activateDynamite();
     }
     /**
      * This method return the power upgrade based on level.
@@ -38,7 +38,32 @@ public final class Dynamite extends PowerInstant {
      *      The Dynamite.
      */
     public static Power create(final int powerLevel) {
-        return new Dynamite();
+        return new Dynamite(powerLevel);
+    }
+
+    private void activateDynamite() {
+        LevelManager.getCurrentLevel().getAllObjects().forEach(obj ->
+        obj.accept(new AbstractGameObjectVisitor<Void>(null) {
+            @Override
+            public Void visit(final Ball ball) {
+                ball.destroy();
+                return null;
+            }
+        }));
+        this.remainingHits--;
+        if (this.remainingHits == 0) {
+            this.deactivate();
+        }
+        this.timeToNextDestroy += WAIT_TIME;
+    }
+
+    @Override
+    public void update(final double deltaTime) {
+        timeToNextDestroy -= deltaTime;
+        if (timeToNextDestroy <= 0) {
+            this.activateDynamite();
+            timeToNextDestroy += WAIT_TIME;
+        }
     }
 }
 
