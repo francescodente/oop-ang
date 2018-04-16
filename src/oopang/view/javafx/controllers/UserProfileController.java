fc85d0b50cb265786f654712360461bbe5883696
FRANCESCA TONETTI <francesca.tonetti@studio.unibo.it>
2018-04-16 22:50:07 +0200

    Add a new method refresh

--- src/oopang/view/javafx/controllers/UserProfileController.java
+++ src/oopang/view/javafx/controllers/UserProfileController.java
@@ -1,12 +1,64 @@
 package oopang.view.javafx.controllers;
 
+import java.util.Arrays;
+
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import oopang.controller.Controller;
+import oopang.controller.users.User;
+import oopang.model.powers.PowerTag;
 import oopang.view.GameScene;
+import oopang.view.View;
+import oopang.view.rendering.javafx.JavaFXUIFactory;
 
 /**
  * This class implements the scene UserProfile.
  */
 public final class UserProfileController extends SceneController {
+
+    @FXML
+    private Label userName;
+    @FXML
+    private Label xpPoints;
+    @FXML
+    private Label coins;
+    @FXML
+    private Label levelRank;
+    @FXML
+    private ProgressBar xpBar;
+    @FXML
+    private VBox powerContainerBox;
+
+    private JavaFXUIFactory factory;
+    private User user;
+
+    @Override
+    public void init(final Controller controller, final View view) {
+        super.init(controller, view);
+        this.user = controller.getUser().get();
+        this.userName.setText(this.user.getName());
+        this.levelRank.setText("Rank: " + this.user.getRank());
+        this.xpBar.setProgress(this.user.getXpLevelPercentage());
+        this.factory = new JavaFXUIFactory();
+        this.user.getCoinsEvent().register(n -> {
+            Platform.runLater(() -> refresh());
+        });
+        refresh();
+    }
+
+    /**
+     * Refresh all the values in the screen.
+     */
+    private void refresh() {
+        this.coins.setText("Coins: " + this.user.getCoins());
+        Arrays.stream(PowerTag.values()).forEach(tag -> {
+            this.powerContainerBox.getChildren().add(this.factory.createUpdatePowerLevelBlock(this.user, tag));
+        });
+    }
 
     @Override
     protected GameScene getNextScene() {
