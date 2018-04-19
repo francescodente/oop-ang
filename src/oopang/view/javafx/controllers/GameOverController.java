881af60bfafb10c6742d8d27c1f2cdfcc477400d
Lorenzo <lorenzo.menghini2@studio.unibo.it>
2018-04-19 08:26:06 +0200

    Styled step scenes of winning and losing
    Add Level12.xml incomplete

--- src/oopang/view/javafx/controllers/GameOverController.java
+++ src/oopang/view/javafx/controllers/GameOverController.java
@@ -1,14 +1,30 @@
 package oopang.view.javafx.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import oopang.controller.Controller;
 import oopang.view.GameScene;
+import oopang.view.View;
 
 /**
  * Describes the controller for the Game Over scene.
  */
 public final class GameOverController extends SceneController {
 
+    @FXML
+    private Label labelScore;
+    @FXML
+    private Label labelName;
+
+    @Override
+    public void init(final Controller controller, final View view) {
+        super.init(controller, view);
+        this.labelScore.setText("Name: " + controller.getUser().get().getName());
+        this.labelName.setText("Score: " + controller.getCurrentTotalScore());
+    }
+    
     @Override
     protected GameScene getNextScene() {
         return GameScene.LEADERBOARD;
--- src/oopang/view/javafx/controllers/LevelResetController.java
+++ src/oopang/view/javafx/controllers/LevelResetController.java
@@ -1,13 +1,29 @@
 package oopang.view.javafx.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import oopang.controller.Controller;
 import oopang.view.GameScene;
+import oopang.view.View;
 
 /**
  * This class implements the scene LevelReset.
  */
 public final class LevelResetController extends SceneController {
+
+    @FXML
+    private Label labelScore;
+    @FXML
+    private Label labelName;
+
+    @Override
+    public void init(final Controller controller, final View view) {
+        super.init(controller, view);
+        this.labelScore.setText("Name: " + controller.getUser().get().getName());
+        this.labelName.setText("Score: " + controller.getCurrentTotalScore());
+    }
 
     @Override
     protected GameScene getNextScene() {
@@ -21,7 +37,7 @@ public final class LevelResetController extends SceneController {
 
     @Override
     public void onKeyPressed(final KeyEvent event) {
-        if (event.getCode() == KeyCode.SPACE) {
+        if (event.getCode() == KeyCode.ENTER) {
             this.nextScene();
         }
     }
