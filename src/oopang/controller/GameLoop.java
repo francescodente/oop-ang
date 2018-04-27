bf4e0e3a4de0d27d44fca15aeeba1811c7aef0c0
Francesco Dente <dente.francesco1996@gmail.com>
2018-04-27 02:11:55 +0200

    Hide game loop implementation behind an interface

--- src/oopang/controller/GameLoop.java
+++ src/oopang/controller/GameLoop.java
@@ -1,103 +1,30 @@
 package oopang.controller;
 
-import java.util.EnumMap;
-import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-
 import oopang.commons.Command;
 import oopang.commons.PlayerTag;
-import oopang.model.Model;
-import oopang.model.input.InputWriter;
-import oopang.view.View;
 
 /**
  * Represents an engine that periodically updates the world and renders it on the view.
  */
-public class GameLoop extends Thread {
-
-    private static final long MS_BETWEEN_FRAMES = 20;
-    private static final double MAX_UPDATE_BOUND = 0.04;
-    private static final double MSEC_TO_SEC = 0.001;
-    private static final int MAXINPUT = 20;
-
-    private final View scene;
-    private final Model world;
-    private final Map<PlayerTag, BlockingQueue<Command>> inputQueue;
-    private final Map<PlayerTag, InputWriter> input;
-    private volatile boolean paused;
-    private volatile boolean stopped;
-
-
+public interface GameLoop {
     /**
-     * Creates a new game loop that updates the given model and renders on the
-     * given view.
-     * @param view
-     *      the view to render on each frame.
-     * @param model
-     *      the model to update each frame.
-     * @param input
-     *      the map with the input Writers.
+     * Enables the game loop.
      */
-    public GameLoop(final View view, final Model model, final Map<PlayerTag, InputWriter> input) {
-        super();
-        this.setName("Game Loop");
-        this.setDaemon(true);
-        this.scene = view;
-        this.world = model;
-        this.paused = false;
-        this.inputQueue = new EnumMap<>(PlayerTag.class);
-        input.keySet().forEach(player -> this.inputQueue.put(player, new ArrayBlockingQueue<>(MAXINPUT)));
-        this.input = input;
-    }
-
-    @Override
-    public void run() {
-        this.updateGame(0);
-        this.render();
-        long lastTime = System.currentTimeMillis();
-        while (!this.stopped) {
-            if (this.paused) {
-                synchronized (this) {
-                    while (this.paused) {
-                        try {
-                            this.wait();
-                        } catch (InterruptedException e) { }
-                    }
-                }
-                lastTime = System.currentTimeMillis() - MS_BETWEEN_FRAMES;
-            }
-            final long current = System.currentTimeMillis();
-            this.processInput();
-            this.updateGame(Math.min((current - lastTime) * MSEC_TO_SEC, MAX_UPDATE_BOUND));
-            this.render();
-            this.waitForNextFrame(current);
-            lastTime = current;
-        }
-    }
-
+    void start();
     /**
      * Temporarily stops the loop until resume() is called.
      */
-    public synchronized void pauseLoop() {
-        this.paused = true;
-    }
+    void pauseLoop();
 
     /**
      * Makes the loop restart if it was previously paused.
      */
-    public synchronized void resumeLoop() {
-        this.paused = false;
-        this.notifyAll();
-    }
+    void resumeLoop();
 
     /**
      * Stops the loop and the running thread.
      */
-    public synchronized void stopLoop() {
-        this.stopped = true;
-        this.interrupt();
-    }
+    void stopLoop();
 
     /**
      * Try to add a new Command to the commandQueue.
@@ -108,35 +35,5 @@ public class GameLoop extends Thread {
      * @return 
      *      true if the command is successful added
      */
-    public synchronized boolean addCommand(final Command cmd, final PlayerTag player) {
-        return this.inputQueue.get(player).offer(cmd);
-    }
-
-    private void processInput() {
-        this.inputQueue.keySet().forEach(player -> {
-            final BlockingQueue<Command> queue = inputQueue.get(player);
-            Command cmd = queue.poll();
-            while (cmd != null) {
-                cmd.execute(input.get(player));
-                cmd = queue.poll();
-            }
-        });
-    }
-
-    private void updateGame(final double deltaTime) {
-        this.world.update(deltaTime);
-    }
-
-    private void render() {
-        this.scene.render();
-    }
-
-    private void waitForNextFrame(final long current) {
-        final long dt = System.currentTimeMillis() - current;
-        if (dt < MS_BETWEEN_FRAMES) {
-            try {
-                Thread.sleep(MS_BETWEEN_FRAMES - dt);
-            } catch (InterruptedException ex) { }
-        }
-    }
+    boolean addCommand(Command cmd, PlayerTag player);
 }
