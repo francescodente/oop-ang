275e70c946cfdf0f24c079df103a828c6be58006
Lorenzo <lorenzo.menghini2@studio.unibo.it>
2018-08-31 01:49:56 +0200

    SIAMO TORNATI MERDEEEE

---
+++ src/oopang/controller/leaderboard/OnlineLeaderboardManager.java
@@ -0,0 +1,31 @@
+package oopang.controller.leaderboard;
+
+import java.util.Optional;
+
+public class OnlineLeaderboardManager implements LeaderboardManager {
+
+    @Override
+    public Optional<Leaderboard> loadStoryModeLeaderboard() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<Leaderboard> loadSurvivalModeLeaderboard() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean saveStoryModeLeaderboard(Leaderboard leaderboard) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean saveSurvivalModeLeaderboard(Leaderboard leaderboard) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
